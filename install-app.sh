1d564fc4f98f5e255ff54d448732a57096e72851
litvi <litvin4uk.roman@gmail.com>
2025-09-30 15:59:51 +0300

    Correction

--- install-app.sh
+++ install-app.sh
@@ -10,6 +10,7 @@
 apt-get update -yq
 apt-get install python3-pip -yq
 
+
 # Create a directory for the app and download the files.
 mkdir /app
 # make sure to uncomment the line bellow and update the link with your GitHub username
